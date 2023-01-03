8f5260907ed6a9fe2f4d2c342cc052eb4cd5bbd9
Panudach Kongsomboon <120073076+AumPanudach@users.noreply.github.com>
2023-01-04 03:59:48 +0700

    Delete maparray.js

--- maparray.js
+++
@@ -1,7 +0,0 @@
-const finalParticipants = ["Taylor","Donal","Don","Natasha","Bobby"];
-
-const announcements = finalParticipants.map((membar) => {
-    return membar + "joined the contest.";
-});
-
-console.log(announcements);
