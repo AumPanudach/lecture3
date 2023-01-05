581dc8d84e0655e4f8a6c823d472e0767212bd20
AumPanudach <6406021611026@fitm.kmutnb.ac.th>
2023-01-05 08:51:28 +0700

    update

--- methodsArray.js
+++ methodsArray.js
@@ -1,5 +1,5 @@
 let arr1 = ["A" , true , 2];
-console.log("\n******Push and pop ******\n");
+console.log("\n******Push and pop******\n");
 console.log(arr1.push("new value"));
 console.log(arr1.pop());
 console.log(arr1);
